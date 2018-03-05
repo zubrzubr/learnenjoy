cff40fae769d7bcd7e2fb7100ea6e21cc653bf86
zubrzubr <maxzub@gmail.com>
2018-03-05 22:36:06 +0200

    Fixed issue with manage py

--- manage.py
+++ manage.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learntoenjoy.settings')
     os.environ.setdefault('DJANGO_CONFIGURATION', 'DevDefaultSite')
 
-    from configurations.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    try:
+        from configurations.management import execute_from_command_line
+    except ImportError:
+        raise ImportError('Something wrong with django configurations package')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
