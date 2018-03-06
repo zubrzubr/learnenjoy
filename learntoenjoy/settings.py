d1214f1beba3f64ca5929ecc98b15babe0f2a29f
zubrzubr <maxzub@gmail.com>
2018-03-06 22:44:01 +0200

    Code style fix in settings

--- learntoenjoy/settings.py
+++ learntoenjoy/settings.py
@@ -1,5 +1,3 @@
-from os.path import join, dirname, abspath, pardir
-
 from learntoenjoy.configs.common import BaseSettings
 from learntoenjoy.configs.mixins import DevMixin, ProdMixin, TestingMixin
 
@@ -7,8 +5,10 @@ from learntoenjoy.configs.mixins import DevMixin, ProdMixin, TestingMixin
 class DevDefaultSite(DevMixin, BaseSettings):
     pass
 
+
 class ProdDefaultSite(ProdMixin, BaseSettings):
     pass
+
 
 class TestingSite(TestingMixin, DevMixin, BaseSettings):
     pass
